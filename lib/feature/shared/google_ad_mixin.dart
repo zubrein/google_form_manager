de66a66701e286a01222ee9564a200bbf173a4e6
robi <robi3443@gmail.com>
2024-03-06 01:35:44 +0600

    Add Google ad test

---
+++ lib/feature/shared/google_ad_mixin.dart
@@ -0,0 +1,59 @@
+import 'dart:io';
+
+import 'package:google_mobile_ads/google_mobile_ads.dart';
+
+import '../../core/helper/logger.dart';
+
+mixin GoogleAdMixin {
+  RewardedInterstitialAd? _rewardedInterstitialAd;
+
+  final String _adUnitId = Platform.isAndroid
+      ? 'ca-app-pub-3940256099942544/5354046379'
+      : 'ca-app-pub-3940256099942544/6978759866';
+
+  void showAdCallback() {
+    _rewardedInterstitialAd?.show(
+        onUserEarnedReward: (AdWithoutView view, RewardItem rewardItem) {
+      loadAd();
+      Log.info('Rewarded');
+    });
+  }
+
+  void loadAd() {
+    RewardedInterstitialAd.load(
+        adUnitId: _adUnitId,
+        request: const AdRequest(),
+        rewardedInterstitialAdLoadCallback:
+            RewardedInterstitialAdLoadCallback(onAdLoaded: (ad) {
+          ad.fullScreenContentCallback = FullScreenContentCallback(
+              // Called when the ad showed the full screen content.
+              onAdShowedFullScreenContent: (ad) {
+            Log.info('Add showed');
+          },
+              // Called when an impression occurs on the ad.
+              onAdImpression: (ad) {
+            Log.info('Impression added');
+          },
+              // Called when the ad failed to show full screen content.
+              onAdFailedToShowFullScreenContent: (ad, err) {
+            Log.info('error $err');
+            ad.dispose();
+          },
+              // Called when the ad dismissed full screen content.
+              onAdDismissedFullScreenContent: (ad) {
+            Log.info('Ad dismissed');
+            ad.dispose();
+          },
+              // Called when a click is recorded for an ad.
+              onAdClicked: (ad) {
+            Log.info('Ad clicked');
+          });
+
+          // Keep a reference to the ad so you can show it later.
+          _rewardedInterstitialAd = ad;
+        }, onAdFailedToLoad: (LoadAdError error) {
+          // ignore: avoid_print
+          print('RewardedInterstitialAd failed to load: $error');
+        }));
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:google_form_manager/feature/form_list/ui/form_list_page.dart';
+import 'package:google_mobile_ads/google_mobile_ads.dart';
 import 'core/di/dependency_initializer.dart';
 import 'core/helper/logger.dart';
 import 'feature/auth/ui/login_page.dart';
 
 void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  MobileAds.instance.initialize();
   configureDependencies();
   Log.init();
   runApp(const MyApp());
